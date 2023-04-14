e5ff19fbe07bc1b91d31da8b8fdfc0d98a380fc4
Sai Kumar Siddu <siddusaikumar2017@gmail.com>
2023-04-14 00:51:32 -0500

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging, logger
+import logging, src.logger
 def error_message_details(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
