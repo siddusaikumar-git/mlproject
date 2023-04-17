f5caa555a98aea908ca87abe117f0fd8493f4c9a
Sai Kumar Siddu <siddusaikumar2017@gmail.com>
2023-04-17 16:34:10 -0500

    prediction pipeline added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,9 +44,47 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostClassifier(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor()
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostClassifier(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
+            }
+
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
             model_report:dict = evaluate_models(
@@ -54,8 +92,8 @@ class ModelTrainer:
                 y_train=y_train,
                 X_test=X_test, 
                 y_test=y_test, 
-                models=models)
-
+                models=models, 
+                params=params)
             
             # To get best model name and value from dictionary
             best_model_name = None
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 def save_object(file_path, obj):
     try:
@@ -20,12 +22,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for model_name, model in models.items():
 
+            if model_name in params:
+                para=params[model_name]
+                gs = GridSearchCV(model,para,cv=3)
+                gs.fit(X_train,y_train)
+                model.set_params(**gs.best_params_)
+                
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -40,14 +48,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
         return report
 
-        # for i in range(len(list(models))):
-        #     model = list(models.values())[i]
-        #     model.fit(X_train, y_train)
-        #     y_train_pred = model.predict(X_train)
-        #     y_test_pred = model.predict(X_test)
-        #     train_model_score = r2_score(y_train, y_train_pred)
-        #     train_model_score = r2_score(y_test, y_test_pred)
-        #     report[list(models.keys())[i]] = test_model_score
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    
     except Exception as e:
         raise CustomException(e, sys)
